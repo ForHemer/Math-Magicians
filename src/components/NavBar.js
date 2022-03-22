27045fc3b1c138e814c11abd9394d0ec1903fb8b
ForHemer <foreste.hemerson@gmail.com>
2022-03-22 14:46:30 -0400

    Create Home, NavBar and Quote.js files

---
+++ src/components/NavBar.js
@@ -0,0 +1,38 @@
+import { NavLink } from 'react-router-dom';
+
+function NavBar() {
+  return (
+    <nav>
+      <div>
+        <NavLink className="logo" to="/math-magicians">
+          Math Magicians
+        </NavLink>
+      </div>
+      <ul>
+        <li>
+          <NavLink className={({ isActive }) => (isActive ? 'active' : '')} to="/math-magicians">
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            className={({ isActive }) => (isActive ? 'active' : '')}
+            to="/Calculator"
+          >
+            Calculator
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            className={({ isActive }) => (isActive ? 'active' : '')}
+            to="/Quote"
+          >
+            Quote
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default NavBar;
